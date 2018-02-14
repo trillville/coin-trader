65f453e5a97f018fe839541da23778c9cc887560
trillville <tillman.elser@gmail.com>
2018-02-13 18:07:34 -0800

    trying different reward func

--- trader/trader.py
+++ trader/trader.py
@@ -4,8 +4,7 @@ import numpy as np
 import json
 
 from argparse import ArgumentParser
-from tensorforce.agents import PPOAgent
-from tensorforce.execution import Runner
+from tensorforce.agents import PPOAgent, RandomAgent
 from tensorforce.core.networks import LayeredNetwork
 import tensorflow as tf
 
@@ -17,15 +16,20 @@ def main(args):
 
         dense = {
             'type': 'dense',
-            'activation': 'tanh',
-            'l2_regularization': 0.0,
-            'l1_regularization': 0.0
+            'activation': 'relu',
+            'l2_regularization': 0.01,
+            'l1_regularization': 0.01
         }
 
         arr = []
 
-        for i in range(1,3):
-            size = 32
+        for i in range(1,2):
+            if i == 1:
+                size = 8
+            elif i == 2:
+                size = 16
+            elif i == 3:
+                size = 8
             arr.append({'size': size, **dense})
 
         print(arr)
@@ -46,16 +50,17 @@ def main(args):
                 series = x['env']
                 stationary = x['agent']
                 x = series
+                x = tf.concat([x, stationary], axis=1)
 
-                apply_stationary_here = 1
+                # apply_stationary_here = 10
                 internal_outputs = list()
                 index = 0
 
                 for i, layer in enumerate(self.layers):
                     layer_internals = [internals[index + n] for n in range(layer.num_internals)]
                     index += layer.num_internals
-                    if i == apply_stationary_here:
-                        x = tf.concat([x, stationary], axis=1)
+                    # if i == apply_stationary_here:
+                    #     x = tf.concat([x, stationary], axis=1)
                     x = layer.apply(x, update, *layer_internals)
 
                     if not isinstance(x, tf.Tensor):
@@ -70,37 +75,54 @@ def main(args):
 
     sample_data = pd.read_csv(args.data_path).values
 
-    env = TradingEnv(data=sample_data)
+    env = TradingEnv(data=sample_data, params=dict(single_action=True))
     network = custom_net()
 
-    agent = PPOAgent(
-        states_spec=env.states,
-        actions_spec=env.actions,
-        network_spec=network,
-        batch_size=500,
-        # BatchAgent
-        keep_last_timestep=True,
-        # PPOAgent
-        step_optimizer=dict(
-            type='adam',
-            learning_rate=1e-3
+    test_random = False
+
+    if test_random == True:
+        agent = RandomAgent(
+            states_spec = env.states,
+            actions_spec = env.actions)
+    else:
+        agent = PPOAgent(
+            states_spec=env.states,
+            actions_spec=env.actions,
+            network_spec=network,
+            batch_size=32,
+            # BatchAgent
+            keep_last_timestep=True,
+            # PPOAgent
+            step_optimizer=dict(
+                type='adam',
+                learning_rate=3e-4
+            ),
+            optimization_steps=10, 
+            # Model
+            scope='ppo', 
+            discount=0.95,
+            # DistributionModel
+            distributions_spec=None, 
+            entropy_regularization=0.01,
+            # PGModel
+            baseline_mode=None, 
+            baseline=None, 
+            baseline_optimizer=None, 
+            gae_lambda=None,
+            # PGLRModel 
+            likelihood_ratio_clipping=0.2, 
+            summary_spec=None, 
+            distributed_spec=None
         )
-    )
 
-    runner = Runner(agent=agent, environment=env)
-
-    def episode_finished(runner):
-        if runner.episode % 100 == 0:
-            print(sum(runner.episode_rewards[-100:]) / 100)
-        return runner.episode < 100 \
-            or not all(reward > 0.0 for reward in runner.episode_rewards[-100:])
-
-    runner.run(episodes=1000, episode_finished=episode_finished)
+    env.train_and_test(agent, num_runs = 5)
+    agent.close()
+    env.close()
 
 def make_parser():
     parser = ArgumentParser()
     parser.add_argument('-d', '--data_path', dest='data_path',
-                        required=False, default="/Users/tillmanelser/crypto/trading/coin-bot/sample_data/sample_data.csv")
+                        required=False, default="/Users/tillmanelser/crypto/trading/coin-bot/sample_data/BTC_DATA.csv")
     parser.add_argument('-n', '--network_spec', dest='network_spec',
                         required=False, default="/Users/tillmanelser/crypto/trading/coin-bot/trader/network_spec.json")
     return parser
--- trader/trading_env.py
+++ trader/trading_env.py
@@ -1,49 +1,66 @@
 from random import random
 import pandas as pd
 import numpy as np
+from collections import Counter
+from math import floor, ceil
+import time
 
 from tensorforce.environments import Environment
-
+from tensorforce.execution import Runner
 
 class TradingEnv(Environment):
     """
     Skeleton of the GDAX trading env
     """
-    def __init__(self, data):
+    def __init__(self, data, params):
         self.data = data # pandas dataframe (currently read from CSV)
+        self.params = params
 
         # normalize TS data
-        self.data[:,0] = self.data[:,0] - self.data[:,0].min()
-        self.data[:,0] = self.data[:,0] / self.data[:,0].max()
-
         # self.data[:,6] = self.data[:,6] - self.data[:,6].min()
         # self.data[:,6] = self.data[:,6] / self.data[:,6].max()
 
-        MIN_TRADE = 0.01
-        MAX_TRADE = 0.1
+        self.MIN_TRADE = 50.0
+        self.MAX_TRADE = 10000.0
 
-        self.START_ETH = 10.0 # number of starting ether
-        self.START_BTC = 1.0 # number of starting bitcoins
-        self.FEE = 0.003 # exchange fee
-        self.MAX_HOLD_LENGTH = 500 # punish if we don't do anything for 500 consecutive timesteps
+        self.START_USD = 10000.0 # number of starting usder
+        self.START_BTC = 10000.0 # number of starting bitcoins
+        self.FEE = 0.00 # exchange fee
+        self.MAX_HOLD_LENGTH = 24 * 60 * 2 # punish if we don't do anything for a day
 
-        self._actions = dict(
-            action=dict(type='int', shape=(), num_actions=3), # buy BTC/sell BTC/hold (do nothing)
-            amount=dict(type='float', shape=(), min_value=MIN_TRADE, max_value=MAX_TRADE)
-        )
+        self.TRAIN_TEST_SPLIT = 0.8 # 80% train, 20% test
+
+        if params['single_action']:
+            self._actions = dict(type='float', shape=(), min_value=-self.MAX_TRADE, max_value=self.MAX_TRADE)
+        else:
+            self._actions = dict(
+                action=dict(type='int', shape=(), num_actions=3), # buy BTC/sell BTC/hold (do nothing)
+                amount=dict(type='float', shape=(), min_value=self.MIN_TRADE, max_value=self.MAX_TRADE)
+            )
 
         self._states = dict(
             env=dict(type='float', shape=self.data.shape[1]),  # environment states
-            agent=dict(type='float', shape=4)  # agent states [eth, btc, repeats, last signal]
+            agent=dict(type='float', shape=4)  # agent states [usd, btc, repeats, last signal]
         )
 
         self.acc = dict(
-            step=0,
-            eth=self.START_ETH,
-            btc=self.START_BTC,
-            repeats=0,
-            signal=0
+            episode=dict(
+                i=0,
+                num_steps=0,
+                advantages=[],
+                uniques=[]
+            ),
+            step = dict(
+                i=0,
+                usd=self.START_USD,
+                btc=self.START_BTC,
+                val=self.START_USD + self.START_BTC,
+                repeats=0,
+                signals=[]
+            )
         )
+
+        self.hold = dict(usd=self.START_USD, btc=self.START_BTC) # this will be our baseline
 
     def __str__(self):
         return 'TradingEnv'
@@ -56,75 +73,165 @@ class TradingEnv(Environment):
         np.random.seed(seed)
 
     def reset(self):
-        self.acc = dict(
-            step=0,
-            eth=self.START_ETH,
-            btc=self.START_BTC,
-            repeats=0,
-            signal=0
-        )
+        self.hold['btc'] = self.START_USD + self.START_BTC
 
-        return self._get_next_state(self.START_ETH, self.START_BTC, 0, 0)
+        ep_acc, step_acc = self.acc['episode'], self.acc['step']
+        step_acc['usd'], step_acc['btc'] = self.START_USD, self.START_BTC
+        step_acc['i'] = 0
+        step_acc['signal'] = [0] * self.run_data.shape[1]
+        step_acc['repeats'] = 0
+        ep_acc['i'] += 1
 
-    def _get_next_state(self, eth, btc, repeats, signal):
-        env = self.data[self.acc['step'],:].tolist()
-        agent = [eth, btc, repeats, signal]
+        return self._get_next_state(self.START_USD, self.START_BTC, 0, 0)
+
+    def _get_next_state(self, usd, btc, repeats, signal):
+        env = self.data[self.acc['step']['i'],:].tolist()
+        agent = [usd, btc, repeats, signal]
 
         return dict(env=env, agent=agent)
 
     def execute(self, actions):
-        signal = {
-            0: -1,  # make amount negative
-            1: 0,  # hold
-            2: 1  # make amount positive
-        }[actions['action']] * actions['amount']
-        if not signal:
-            signal = 0
+        if self.params['single_action']:
+            signal = 0 if -self.MIN_TRADE < actions < self.MIN_TRADE else actions
+        else:
+            signal = {
+                0: -1,  # make amount negative
+                1: 0,  # hold
+                2: 1  # make amount positive
+            }[actions['action']] * actions['amount']
+            if not signal:
+                signal = 0
+
         abs_sig = abs(signal)
 
-        reward = 0 # initialize reward
+        step_acc, ep_acc = self.acc['step'], self.acc['episode']
 
-        # initialize last/curr eth/btc values and number of repeated actions
-        last_eth, last_btc, last_signal = self.acc['eth'], self.acc['btc'], self.acc['signal']
-        last_price = self.data[self.acc['step'],1] # initialize price
-        last_btc_value = last_btc + last_eth * last_price
+        # initialize last/curr usd/btc values and number of repeated actions
+        last_usd, last_btc, last_signal = step_acc['usd'], step_acc['btc'], step_acc['signal']
 
-        if signal > 0 and not abs_sig > last_eth:
-            self.acc['btc'] = last_btc + abs_sig - (abs_sig * self.FEE)
-            self.acc['eth'] = last_eth - abs_sig / last_price
+        if signal > 0 and not abs_sig > last_usd:
+            step_acc['btc'] += (1.0 - self.FEE) * abs_sig
+            step_acc['usd'] -= abs_sig
         elif signal < 0 and not abs_sig > last_btc:
-            self.acc['btc'] = last_btc - abs_sig
-            self.acc['eth'] = last_eth + (abs_sig - abs_sig * self.FEE) / last_price
+            step_acc['btc'] -= abs_sig
+            step_acc['usd'] += (1.0 - self.FEE) * abs_sig
 
-        # now increment time (probably a better way to do this without using a global var...)
-        self.acc['step'] += 1
-        self.acc['signal'] = signal
+        step_acc['signal'] = signal
+        step_acc['i'] += 1
 
-        curr_price = self.data[self.acc['step'],1]
+        # Treating a buy and hold strategy as the baseline
+        pct_change = self.price_pct_changes[step_acc['i']]
+        step_acc['btc'] += pct_change * step_acc['btc']
+        total = step_acc['usd'] + step_acc['btc']
 
-        # not sure if this is the best comparison... could also compare to reward of holding
-        curr_btc_value = self.acc['btc'] + self.acc['eth'] * curr_price
-        reward = curr_btc_value - last_btc_value
+        # see how the buy and hold strategy would have done
+        hold = self.hold
+        hold['btc'] += pct_change * hold['btc']
+        reward = (total - hold['btc']) / total
 
         # Collect repeated same-action count (homogeneous actions punished below)
-        if signal == last_signal: # no change in signal
-            self.acc['repeats'] += 1
+        recent_actions = np.array(step_acc['signals'][-step_acc['repeats']:])
+        if np.any(recent_actions > 0) and np.any(recent_actions < 0) and np.any(recent_actions == 0):
+            step_acc['repeats'] = 1  # reset repeat counter
         else:
-            self.acc['repeats'] = 1
+            step_acc['repeats'] = 1
 
-        if self.acc['step'] % 100 == 0:
-            print("step: ", self.acc['step'], "eth: ", self.acc['eth'], "btc: ",self.acc['btc'], "tot val: ", curr_btc_value, "reward:", reward, "repeats: ", self.acc['repeats'], "signal: ", signal)
+        #if step_acc['i'] % 1000 == 0:
+        #    print("step: ", step_acc['i'], "usd: ", step_acc['usd'], "btc: ", step_acc['btc'], "tot val: ", total, "reward:", reward, "repeats: ", step_acc['repeats'], "signal: ", signal)
 
         # this step has to be after the time increment!
-        next_state = self._get_next_state(self.acc['eth'], self.acc['btc'], self.acc['repeats'], signal)
-        self.acc['signal'] = signal
+        next_state = self._get_next_state(step_acc['usd'], step_acc['btc'], step_acc['repeats'], signal)
+        step_acc['signal'] = signal
 
-        terminal = self.acc['step'] >= len(self.data) - 1
-        if self.acc['repeats'] >= self.MAX_HOLD_LENGTH:
+        terminal = step_acc['i'] >= len(self.run_data) - 1
+        if step_acc['repeats'] >= self.MAX_HOLD_LENGTH:
             reward -= -1.0  # start trading u cuck
             terminal = True
 
+        if terminal is True:
+            self.episode_finished()
+
         return next_state, terminal, reward
+
+    def episode_finished(self):
+        step_acc, ep_acc = self.acc['step'], self.acc['episode']
+        signals = step_acc['signals']
+
+        advantage = (step_acc['btc'] + step_acc['usd']) - (self.hold['btc'])
+
+        ep_acc['advantages'].append(advantage)
+        n_uniques = float(len(np.unique(signals)))
+        ep_acc['uniques'].append(n_uniques)
+
+        # Print (limit to note-worthy)
+        common = dict((round(k,2), v) for k, v in Counter(signals).most_common(5))
+        completion = f"|{int(ep_acc['num_steps'] / self.run_data.shape[0] * 100)}%"
+        print("hold BTC: ", self.hold['btc'], " trader value: ", step_acc['btc'] + step_acc['usd'])
+
+        print(f"{ep_acc['i']}|⌛:{step_acc['i']}{completion}\tA:{'%.3f'%advantage}\t{common}({n_uniques}uniq)")
+
+        return True
+
+    ## TODO: setup the run_sim and train_and_test functions. goal -> iterate through the dataset, making num_tests number of unique datasets
+    ## for each dataset, do a train/test exercise and calculate/log the advantage
+    def run_sim(self, runner, print_results=True):
+        next_state, terminal = self.reset(), False
+        while not terminal:
+            next_state, terminal, reward = self.execute(runner.agent.act(next_state, deterministic=True))
+        if print_results: self.episode_finished()
+
+    def train_and_test(self, agent, num_runs):
+        runner = Runner(agent=agent, environment=self)
+        i = 0
+        train, test = self.initialize_data()
+        run_length = 24*60 # one week
+        start_index = 0
+        try:
+            while i <= num_runs:
+                print(test.shape[0])
+                if start_index + run_length >= train.shape[0] - 1:
+                    start_index = 0
+                self.run_data = train[start_index:start_index + run_length, ]
+                print("START INDEX: ", start_index)
+                runner.run(timesteps=self.run_data.shape[0], max_episode_timesteps=self.run_data.shape[0])
+                self.run_data = test
+                self.run_sim(runner)
+                #self.run_data = test
+                #runner.run(timesteps=test.shape[0], max_episode_timesteps=test.shape[0])
+                #self.run_sim(runner, print_results=True)
+                i += 1
+                start_index += run_length
+                print(i)
+                #self.run_sim(runner, print_results=True)
+        except KeyboardInterrupt:
+            self.run_data = test
+            runner.run(max_episode_timesteps=test.shape[0])
+            # Lets us kill training with Ctrl-C and skip straight to the final test. This is useful in case you're
+            # keeping an eye on terminal and see "there! right there, stop you found it!" (where early_stop & n_steps
+            # are the more musdodical approaches)
+            pass
+
+    def _get_diff(self, col, percent=False):
+        series = pd.Series(col)
+        diff = series.pct_change() if percent else series.diff()
+        diff.iloc[0] = 0  # always NaN, nothing to compare to
+
+        # Remove outliers (turn them to NaN)
+        q = diff.quantile(0.99)
+        diff = diff.mask(diff > q, np.nan)
+
+        # then forward-fill the NaNs.
+        return diff.replace([np.inf, -np.inf], np.nan).ffill().bfill().values
+
+    def initialize_data(self):
+        """
+        Fetch and transform train/test data. Note - we are not using random sampling because it is timeseries data
+        """
+        self.price_pct_changes = self._get_diff(self.data[:,2], True)
+        row_cnt = self.data.shape[0]
+        n_train = ceil(row_cnt * self.TRAIN_TEST_SPLIT)
+        train, test = self.data[0:n_train,], self.data[n_train:row_cnt,]
+        return train, test
 
     @property
     def states(self):
